881fc589801e4ba532d95ab77593a21938f32fd1
Aniket Pradhan <aniket17133@iiitd.ac.in>
2018-11-02 14:15:46 +0530

    Fixed some bugs

--- src/Snek/Main.java
+++ src/Snek/Main.java
@@ -287,7 +287,7 @@ public class Main extends Application{
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
 }
